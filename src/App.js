fa2285eecda1b2e4bd92adce79d396ffb174bffb
ElderNobrega <eldernguerra.filho@hotmail.com>
2021-09-09 15:23:42 -0400

    Adding actions creator, redux thunk

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ import { useSelector, useDispatch } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import {uiActions} from './store/ui-slice'
 import Notification from './components/UI/Notification';
-
+import { sendCartData } from './store/cart-slice'
 let isInitial = true
 
 function App() {
@@ -17,34 +16,11 @@ function App() {
   const notification = useSelector(state => state.ui.notification)
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(uiActions.showNotification({
-        status: 'Success',
-        title: 'Success',
-        message: 'Sending cart data successfully'
-      }))
-      const response = await fetch('https://redux-practice-1f0a9-default-rtdb.firebaseio.com/cart.json', {
-        method: 'PUT',
-        body: JSON.stringify(cart)
-      })
-
-      if (!response.ok) {
-        throw new Error('sending cart data failed')
-      }
-    }
-
     if (isInitial) {
       isInitial = false
       return
     }
-    
-    sendCartData().catch((error) => {
-      dispatch(uiActions.showNotification({
-        status: 'error',
-        title: 'Error',
-        message: 'Sending cart data failed'
-      }))
-    })
+    dispatch(sendCartData(cart))
   }, [cart, dispatch])
 
   return (
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { uiActions } from "./ui-slice";
 
 const cartSlice = createSlice({
   name: 'cart',
@@ -37,6 +38,45 @@ const cartSlice = createSlice({
     }
   }
 })
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(uiActions.showNotification({
+      status: 'pending',
+      title: 'Sending...',
+      message: 'Sending cart data'
+    }))
+
+    const sedRequest = async () => {
+      const response = await fetch('https://redux-practice-1f0a9-default-rtdb.firebaseio.com/cart.json', {
+          method: 'PUT',
+          body: JSON.stringify(cart)
+      })
+
+        if (!response.ok) {
+          throw new Error('sending cart data failed')
+        }
+    }
+
+    try {
+      await sedRequest()
+      dispatch(uiActions.showNotification({
+        status: 'success',
+        title: 'success',
+        message: 'Sent cart data successfully'
+      }))
+    } catch (error) {
+        dispatch(uiActions.showNotification({
+          status: 'error',
+          title: 'Error',
+          message: 'Sending cart data failed'
+          })
+        )
+      } 
+
+      
+  }
+}
 
 export const cartActions = cartSlice.actions
 
